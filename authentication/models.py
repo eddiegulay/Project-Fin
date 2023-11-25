3f37ae5d0301a6fa617f8aa56c687cc9cfb8c93f
eddygulled <edgargulay69@gmail.com>
2023-11-25 13:04:58 +0300

    Add privilege and profile models

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+
+class privilege(models.Model):
+    privilege_name = models.CharField(max_length=50)
+
+    def __str__(self):
+
+        return self.privilege_name
+
+
+class Profile(models.Model):
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    privilege = models.ForeignKey(privilege, on_delete=models.CASCADE, default=1)
+
+    def __str__(self):
+        return self.user.username
+
