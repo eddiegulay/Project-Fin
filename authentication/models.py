a73567c34bbc780998b00d81c4dc1c48a51db660
eddygulled <edgargulay69@gmail.com>
2023-11-25 13:05:28 +0300

    Fix import statement in models.py

--- authentication/models.py
+++ authentication/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -12,7 +13,7 @@ class privilege(models.Model):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     privilege = models.ForeignKey(privilege, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
