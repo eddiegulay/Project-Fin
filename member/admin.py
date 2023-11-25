41aceeec3249d72ca658771bd616771db8d0ec62
eddygulled <edgargulay69@gmail.com>
2023-11-26 01:07:55 +0300

    Add admin models and views for Bio, Project, and ProjectFile

--- member/admin.py
+++ member/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Bio)
+class BioAdmin(admin.ModelAdmin):
+    list_display = ('user', 'reg_number', 'programme', 'linked_in', 'github')
+    search_fields = ('user__username', 'reg_number', 'programme')
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'end_date', 'is_completed')
+    search_fields = ('title', 'description')
+
+@admin.register(ProjectFile)
+class ProjectFileAdmin(admin.ModelAdmin):
+    list_display = ('project', 'description', 'uploaded_by', 'uploaded_at')
+    search_fields = ('project__title', 'description')
--- member/views.py
+++ member/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
-
+from .models import *
 
 def member_profile(request):
-    return render(request, 'member/profile.html')
+    user = request.user
+    bio = Bio.objects.get(user=user)
+    projects = Project.objects.filter(members=user)
+
+
+    # success rate for project compeltion
+    success_rate = 0
+    if projects.count() > 0:
+        success_rate = (projects.filter(is_completed=True).count() / projects.count()) * 100
+
+    context = {
+        'bio': bio,
+        'projects': projects,
+        'project_count': projects.count(),
+        'success_rate': success_rate,
+    }
+    return render(request, 'member/profile.html', context)
