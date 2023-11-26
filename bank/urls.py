b7a7526eda925236bd20d89339d9e300bf86bbb3
eddygulled <edgargulay69@gmail.com>
2023-11-26 03:04:18 +0300

    Add challenges endpoint to bank URLs

--- bank/urls.py
+++ bank/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', thematic_areas, name='thematic_areas'),
+    path('<int:thematic_area_id>/challenges', challenges, name='challenges'),
 ]
--- bank/views.py
+++ bank/views.py
@@ -6,8 +6,30 @@ def thematic_areas(request):
     thematic_areas = ThematicArea.objects.all()
     Challenges = Challenge.objects.all()
 
+    # get all collaborators for each challenge
+    collaborators = []
+    for challenge in Challenges:
+        challenge_collaborators = challenge.collaborators.all()
+        for collaborator in challenge_collaborators:
+            collaborators.append(collaborator)
+
+    collaborators = list(set(collaborators))
+
     context = {
         'thematic_areas': thematic_areas,
         'Challenges': Challenges,
+        'collaborators': collaborators,
+    }
+    return render(request, 'bank/thematic_areas.html', context)
+
+
+def challenges(request, thematic_area_id):
+    thematic_areas = ThematicArea.objects.all()
+    thematic_area = ThematicArea.objects.get(id=thematic_area_id)
+    challenges = Challenge.objects.filter(thematic_area=thematic_area)
+    context = {
+        'thematic_areas': thematic_areas,
+        'challenges': challenges,
+        'thematic_area': thematic_area,
     }
     return render(request, 'bank/thematic_areas.html', context)
