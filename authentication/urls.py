2e75277715ffafa967dfbb35173f2e5494974ce9
eddygulled <edgargulay69@gmail.com>
2023-11-26 13:42:11 +0300

    create account for employee bio bug occur when selecting create finhub member

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 
 urlpatterns = [
+    path('', register_view, name='register'),
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('logout/', user_logout, name='logout'),
